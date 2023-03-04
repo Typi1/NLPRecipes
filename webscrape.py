d01ccb24383955f356f5d8454767fbff55591f5a
Sean <swe.seanlee@gmail.com>
2023-03-04 16:56:23 -0600

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -9,31 +9,43 @@ def get_soup(url:str):
 def get_dict(url:str):
     recipe_html = get_soup(url)
     recipe_dict = None
+    d_type = None
     candidate_json = recipe_html.find_all("script", {"type": "application/ld+json"})
     for candidate in candidate_json:
         r = json.loads(candidate.text)
-        # print(r)
-        try:
+        # print(type(r))
+        if isinstance(r, dict):
             if r["@type"] == "Recipe":
                 recipe_dict = r
-        except:
-            if r[0]["@type"] == ["Recipe"]:
+                d_type = dict
+                break
+        elif isinstance(r, list):
+            if r[0]["@type"][0] == "Recipe":
                 recipe_dict = r
-    if not recipe_dict:
-        print("Recipe not found!")
-        return None
-    else:
-        return recipe_dict
+                d_type = list
+                break
+            elif r[0]["@type"] == "Recipe":
+                recipe_dict = r
+                d_type = list
+                break
+    return [recipe_dict, d_type]
 
-def get_recipe(url):
+def get_recipe(url:str):
     recipe = get_dict(url)
     # print(recipe)
-    try:
-        ingredients = recipe["recipeIngredient"]
-        steps = recipe["recipeInstructions"]
-    except:
-        ingredients = recipe[0]["recipeIngredient"]
-        steps = recipe[0]["recipeInstructions"]
+    if recipe[0]:
+        if recipe[1] == dict:
+            ingredients = recipe[0]["recipeIngredient"]
+            if "itemListElement" in recipe[0]["recipeInstructions"]:
+                steps = recipe[0]["recipeInstructions"]["itemListElement"]
+            else:
+                steps = recipe[0]["recipeInstructions"]
+        elif recipe[1] == list:
+            ingredients = recipe[0][0]["recipeIngredient"]
+            if "itemListElement" in recipe[0][0]["recipeInstructions"][0]:
+                steps = recipe[0][0]["recipeInstructions"][0]["itemListElement"]
+            else:
+                steps = recipe[0][0]["recipeInstructions"]
     instructions = []
     for step in steps:
         if "text" in step:
@@ -43,8 +55,13 @@ def get_recipe(url):
     return [ingredients, instructions]
 
 def main():
-    url = 'https://tasty.co/recipe/fried-egg-pizza'
+    url = 'https://www.simplyrecipes.com/recipes/strawberry_shortcake/'
     get_recipe(url)
 
 if __name__ == "__main__":
     main()
+
+# https://tasty.co/recipe/fried-egg-pizza
+# https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/
+# https://www.simplyrecipes.com/recipes/strawberry_shortcake/
+# https://www.foodnetwork.com/recipes/food-network-kitchen/classic-shrimp-scampi-8849846
